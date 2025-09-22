c113718834950defcabeb396817e4e1ad43dad88
BraydenI <bi1012440@otc.edu>
2025-09-22 10:05:43 -0500

    made starter

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Boostrap CSS -->
+    <?php include 'includes/bootstrapcdnlinks.php'; ?>
+    <title>Document</title>
+</head>
+<body>
+    <!-- Include the navigation bar -->
+    <?php include 'includes/navigation.php'; ?>
+
+    <!-- Include welcome message -->
+    <?php include 'welcome.php;' ?>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,2 @@
+<h1>Welcome to the Movie Club</h1>
+<p>Join us to explore and discuss a wide variety of films!</p>
